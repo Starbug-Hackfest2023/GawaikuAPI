de7810285248e86519ae1fe7cba015dd10972336
Shidqi Yusri <shidqifadhlu@gmail.com>
2024-01-15 17:28:10 +0700

    fix bug in db, and add email detector helpers

--- bin/helpers/databases/mongodb/db.js
+++ bin/helpers/databases/mongodb/db.js
@@ -153,8 +153,8 @@ class DB {
             const parameterSort = {};
             parameterSort[fieldName] = 1;
             const parameterPage = row * (page - 1);
-            // const recordset = await db.find(param).sort(parameterSort).limit(row).skip(parameterPage)
-            const recordset = await db.find(param)
+            // const recordset = await db.find(param)
+            const recordset = await db.find(param).sort(parameterSort).limit(row).skip(parameterPage)
                 .toArray();
             if (validate.isEmpty(recordset)) {
                 return wrapper.error('Data Not Found, Please Try Another Input');
---
+++ bin/helpers/utils/emailDetector.js
@@ -0,0 +1,4 @@
+module.exports.isValidEmail= (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
